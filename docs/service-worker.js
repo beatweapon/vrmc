5ea533125ff9bf11274b9cfbc2c45187c3fc4aaa
Kento Ueda <beatweapon7th@gmail.com>
2024-07-29 22:23:58 +0900

    サービスワーカーの更新処理を実装

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,16 +1,39 @@
+const CACHE_NAME = "static-cache-v1.3.0";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./indexedDB.js",
+  "./assets/icons/icon_180.png",
+  "./assets/icons/icon_192.png",
+  "./assets/icons/icon_512.png",
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("static-cache-v1.2.0").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./indexedDB.js",
-        "./assets/icons/icon_180.png",
-        "./assets/icons/icon_192.png",
-        "./assets/icons/icon_512.png",
-      ]);
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Pre-caching offline page");
+      return cache.addAll(FILES_TO_CACHE);
     }),
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Activating new service worker...");
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            console.log("Deleting old cache:", key);
+            return caches.delete(key);
+          }
+        }),
+      );
+    }),
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
